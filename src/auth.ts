36812341f6b94025848bcdeee8e9983425f5f445
glzaboy <glzaboy@163.com>
2025-03-06 18:47:29 +0800

    add env

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,5 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare"
 import NextAuth from "next-auth"
-import Resend from "next-auth/providers/resend"
-
-const cloudflareCtx = getCloudflareContext({ async: false });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Resend],
-    secret: cloudflareCtx.env.AUTH_SECRET,
+    providers: [],
 })
