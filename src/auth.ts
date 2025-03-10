dd7a97963f30d2bf90863338af00591fad9d1457
glzaboy <glzaboy@163.com>
2025-03-10 22:47:50 +0800

    增加d1database集成

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
-
+import { D1Adapter } from "@auth/d1-adapter"
+import { getCloudflareContext } from "@opennextjs/cloudflare"
+// 用函数延迟获取适配器
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [],
+    adapter: D1Adapter(process.env.DB)
 })
