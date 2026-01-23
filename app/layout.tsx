66393bb86a5598d52d9c68c259ebc819ddc39978
glzaboy <glzaboy@163.com>
2026-01-23 23:34:49 +0800

    升级软件版本

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import Script from 'next/script'; // 1. 导入Script组件
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,6 +30,16 @@ export default function RootLayout({
           {children}
           <Footer />
         </NextThemesProvider>
+        {/* 51LA 统计代码 */}
+        <Script
+          id="51la-analytics"
+          strategy="afterInteractive" // 或 lazyOnload
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3OlCDQyiCiTMOEaG",ck:"3OlCDQyiCiTMOEaG"});
+            `,
+          }}
+        />
       </body>
     </html >
   );
